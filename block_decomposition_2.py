b4575a8c2f51885984e392e2113bf2885376bf29
vladimir.dmitriev <vladimir.dmitriev@clip-login-1.cbe.vbc.ac.at>
2022-04-08 10:43:06 +0200

    update block decomposition

--- block_decomposition_2.py
+++ block_decomposition_2.py
@@ -28,7 +28,7 @@ The only difference is block structure of eigendecomposition
 """
 
 
-def cis_eig_blockwise(c, 
+def cys_eig_blockwise(c, 
                       view_df=None,
                       balance=True,
                       block_size=10000,
@@ -45,8 +45,6 @@ def cis_eig_blockwise(c,
                       bad_bins=None):
     
     """
-    TODO: comply with the numpydoc style, like other cooltools functions
-    
     Compute eigenvectors for cis, uploading matrix blockwise
     
     c - cooler
@@ -84,7 +82,7 @@ def cis_eig_blockwise(c,
     
     
     
-    compute_eig = lambda reg, loc_expected: _cis_eig_blockwise(c=c, 
+    compute_eig = lambda reg, loc_expected: _cys_eig_blockwise(c=c, 
             _region=reg, 
             balance=balance,
             S=loc_expected,
@@ -135,7 +133,7 @@ def cis_eig_blockwise(c,
     return eigvals_table, eigvec_table
 
 
-def _cis_eig_blockwise(c, 
+def _cys_eig_blockwise(c, 
             _region, 
             balance,
             S,
@@ -263,8 +261,8 @@ def compute_mask_and_mean(c,
     
     mask = np.empty(region_shape[0])
     
-    N = region_boundaries[1] - region_boundaries[0]
-    part = N // block_size
+    N = region_boundaries[1]-region_boundaries[0]
+    part = N//block_size
     
     for x in range(0, part+1):
         _block = c.matrix(balance = balance,sparse=False)[region_boundaries[0]+x*block_size:region_boundaries[0]+min((x+1)*block_size, N),region_boundaries[2]:region_boundaries[3]]
@@ -409,13 +407,13 @@ def BlockMult(block_size,
         
             _block[:, bad_bins_region] = np.nan
         
-            bad_bins_region -= shift
-            bad_bins_region = bad_bins_region[(bad_bins_region>=0) & (bad_bins_region<_block.shape[0])]
+            bad_bins_region -=shift
+            bad_bins_region=bad_bins_region[(bad_bins_region>=0) & (bad_bins_region<_block.shape[0])]
             _block[bad_bins_region, :] = np.nan
     
     #_row, row_sum = fetch_row(i, c, fetching_params)#
     #fix Nans/ infs with zeros
-    _block[~np.isfinite(_block)] = 0
+    _block[~np.isfinite(_block)] = 0#### sparsed
         
         
     #ignoring the diags
